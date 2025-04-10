3a9034187f7da8a18c4c5767b797cd537b6c7e8d
lucas-dev79 <86043027+lucas-dev79@users.noreply.github.com>
2025-04-10 10:11:23 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,3 +10,12 @@ python3 manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python3 manage.py migrate
+
+
+# Create superuser if it doesn't exist
+python3 manage.py shell << EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='vod25').exists():
+    User.objects.create_superuser('vod25', password='!@25Loop')
+EOF
